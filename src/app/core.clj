49aac2be0b2a90863808b971fdf4c671aaca6211
Remo Arpagaus <arpagaus.remo@gmail.com>
2015-11-05 22:23:51 +0100

    Added protobuf-compute-size

--- src/app/core.clj
+++ src/app/core.clj
@@ -1,9 +1,14 @@
-(ns app.core)
+(ns app.core
+  (:import (com.google.protobuf CodedOutputStream)))
 
 (defn protobuf-load [data] 5)
 
 (defn protobuf-dump
-      [m]
+      [schema m]
       (seq (byte-array [(unchecked-byte 0x08)
                    (unchecked-byte 0x96)
                    (unchecked-byte 0x01)])))
+
+(defn protobuf-compute-size
+  [schema message]
+  (CodedOutputStream/computeInt32Size 1 (:age message)))
--- test/app/test/core.clj
+++ test/app/test/core.clj
@@ -7,9 +7,18 @@
   (is (= 7 (+ 3 4)))
   (is (= 5 (protobuf-load 5))))
 
+(def simple-schema
+     {:type :message
+      :name "Person"
+      :content [{:type :int32 :name "age" :tag 1}]})
+(def simple-message {:age 150})
+
 (deftest protobuf-dump-test
   (is (= (seq (byte-array [(unchecked-byte 0x08)
                       (unchecked-byte 0x96)
                       (unchecked-byte 0x01)]))
-         (protobuf-dump {:age 150}))))
+         (protobuf-dump simple-schema simple-message))))
 
+(deftest protobuf-compute-size-test
+  (is (= 3 (protobuf-compute-size simple-schema simple-message)))
+  (is (= 4 (protobuf-compute-size simple-schema {:age 150}))))
