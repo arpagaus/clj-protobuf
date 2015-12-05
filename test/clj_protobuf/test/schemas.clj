886c8f13e7d04eaa3218756353f5b868318c3922
Remo Arpagaus <arpagaus.remo@gmail.com>
2015-12-05 21:58:24 +0100

    Automatic indentation

--- test/clj_protobuf/test/schemas.clj
+++ test/clj_protobuf/test/schemas.clj
@@ -8,30 +8,27 @@
 (def schema-simple
   [{:name "Person"
     :type :message
-    :content
-      [{:label :required :type :string :name "name" :tag 1}
-      {:label :required :type :int32 :name "age" :tag 2}
-      {:label :required :type :string :name "email" :tag 3}]}])
+    :content [{:label :required :type :string :name "name" :tag 1}
+              {:label :required :type :int32 :name "age" :tag 2}
+              {:label :required :type :string :name "email" :tag 3}]}])
 
 ;; TODO schema-intermediate
 
-(def schema-advanced [
-  {:type :message :name "Person" :content [
-    {:label :required :name "name" :type :string :tag 1}
-    {:label :required :name "age" :type :int32 :tag 2}
-    {:label :required :name "email" :type :string :tag 3}
-    {:name "Phone" :type :message :content [
-      {:name "PhoneType" :type :enum :content [
-        {:name "MOBILE" :tag 1}
-        {:name "WORK" :tag 2}
-        {:name "HOME" :tag 3}
-      ] }
-      {:label :required :name "number" :type :string :tag 1}
-      {:label :required :name "type" :type "PhoneType" :tag 2}
-    ] }
-    {:label :repeated :name "phone" :type "Phone" :tag 4}
-  ] }
-  {:type :message :name "AnyMessage" :content [
-    {:label :required :name "anystring" :type :string :tag 1}
-  ] }
-])
+(def schema-advanced
+  [{:type :message
+    :name "Person"
+    :content [{:label :required :name "name" :type :string :tag 1}
+              {:label :required :name "age" :type :int32 :tag 2}
+              {:label :required :name "email" :type :string :tag 3}
+              {:name "Phone" :type :message
+               :content [{:name "PhoneType" :type :enum
+                          :content [{:name "MOBILE" :tag 1}
+                                    {:name "WORK" :tag 2}
+                                    {:name "HOME" :tag 3}]}
+                         {:label :required :name "number" :type :string :tag 1}
+                         {:label :required :name "type" :type "PhoneType" :tag 2}]}
+              {:label :repeated :name "phone" :type "Phone" :tag 4}]}
+   {:type :message
+    :name "AnyMessage"
+    :content [{:label :required :name "anystring" :type :string :tag 1}]}
+   ])
