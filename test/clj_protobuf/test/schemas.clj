2a8dfa1011692f8782eb24b6cf6b7ab53a6336a9
Remo Arpagaus <arpagaus.remo@gmail.com>
2015-12-07 08:12:36 +0100

    Added schema-intermediate

--- test/clj_protobuf/test/schemas.clj
+++ test/clj_protobuf/test/schemas.clj
@@ -12,7 +12,16 @@
               {:label :required :type :int32 :name "age" :tag 2}
               {:label :required :type :string :name "email" :tag 3}]}])
 
-;; TODO schema-intermediate
+(def schema-intermediate
+  [{:name "Person"
+    :type :message
+    :content [{:label :required :type :string :name "name" :tag 1}
+              {:label :required :type :int32 :name "age" :tag 2}
+              {:label :required :type :string :name "email" :tag 3}
+              {:label :optional :type :string :name "personType" :tag 4}
+              {:name "PersonType" :type :enum
+               :content [{:name "CUSTOMER" :tag 1}
+                         {:name "PROSPECT" :tag 2}]}]}])
 
 (def schema-advanced
   [{:type :message
